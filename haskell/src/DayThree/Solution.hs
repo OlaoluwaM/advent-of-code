b460d4f9b739ace03b65216fc29b1ac84fbf1ca5
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2023-06-24 07:12:54 -0500

    style: Day 3 solution minor cleanup

--- haskell/src/DayThree/Solution.hs
+++ haskell/src/DayThree/Solution.hs
@@ -4,8 +4,8 @@ import Data.Map qualified as Map
 import Data.Set qualified as Set
 import PyF
 
-splitHalf :: String -> (String, String)
-splitHalf strToSplit = splitAt (div (length strToSplit + 1) 2) strToSplit
+splitInHalf :: String -> (String, String)
+splitInHalf strToSplit = splitAt (div (length strToSplit + 1) 2) strToSplit
 
 maybeToEither :: a -> Maybe b -> Either a b
 maybeToEither _ (Just b) = Right b
@@ -14,17 +14,18 @@ maybeToEither a Nothing = Left a
 mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple f (x, y) = (f x, f y)
 
-getPriority :: Char -> Either String Integer
-getPriority ch =
+getCharPriority :: Char -> Either String Integer
+getCharPriority ch =
   let priorityMap = Map.fromList $ zip ['a' .. 'z'] [1 .. 26] ++ zip ['A' .. 'Z'] [27 .. 52]
       errorMsg = [fmt|Error, could not discern priority value for char: {ch}|]
    in maybeToEither errorMsg (Map.lookup ch priorityMap)
 
 -- Exported for testing purposes
 getIntersectionPrioritySum :: String -> Either String Integer
-getIntersectionPrioritySum = (sum <$>) . mapM (Set.foldl accumPrioritiesOfCommonElems (return 0) . uncurry Set.intersection . mapTuple Set.fromList . splitHalf) . words
+getIntersectionPrioritySum = (sum <$>) . mapM (Set.foldl accumPrioritiesOfCommonElems (return 0) . filterOutCommonElements . splitInHalf) . words
   where
-    accumPrioritiesOfCommonElems acc v = let charEither = getPriority v in (+) <$> charEither <*> acc
+    accumPrioritiesOfCommonElems acc v = let charPriority = getCharPriority v in (+) <$> charPriority <*> acc
+    filterOutCommonElements = uncurry Set.intersection . mapTuple Set.fromList
 
 main :: String -> IO ()
 main inpStr = let output = either id show (getIntersectionPrioritySum inpStr) in print [fmt|Result: {output}|]
